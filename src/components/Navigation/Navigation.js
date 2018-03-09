9935911306b0fa3920a9f690712e73229fda1b66
ravi2808 <ravi.cool2808@gmail.com>
2018-03-09 12:12:19 +0530

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,1 +1,16 @@
+import React from 'react';
 
+import classes from './Navigation.css'
+
+const Navigation = (props) => {
+    return (
+        <nav className={classes.Navigation}>
+            <ul>
+                <li><a>Restaurants</a></li>
+                <li><a>Orders</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
