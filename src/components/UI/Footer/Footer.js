92091b782c2339eb6e02eb469b5a69c8efff6e1c
ravi2808 <ravi.cool2808@gmail.com>
2018-03-09 12:14:15 +0530

    Update Footer.js

--- src/components/UI/Footer/Footer.js
+++ src/components/UI/Footer/Footer.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+import classes from './Footer.css';
+
+const Footer = (props) => {
+    return (
+        <footer className={classes.Footer}>
+            <div className={classes.Total}>Total: <strong>{props.price}</strong></div>
+        </footer>
+    );
+};
+
+export default Footer;
