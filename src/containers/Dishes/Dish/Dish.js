360dc60cceef2c9f6f96aec6b6dc6ac35af58015
ravi2808 <ravi.cool2808@gmail.com>
2018-03-09 12:23:26 +0530

    Update Dish.js

--- src/containers/Dishes/Dish/Dish.js
+++ src/containers/Dishes/Dish/Dish.js
@@ -1,1 +1,29 @@
+import React, { Component } from 'react';
 
+import Button from '../../../components/UI/Button/Button';
+import classes from './Dish.css';
+
+class Dish extends Component {
+    render() {
+        return (
+            <div className={classes.Dish}>
+                <div className={classes.Main}>
+                    <div className={classes.DishName}>{this.props.name}</div>
+                    <div className={classes.Quantity}>
+                        <Button disable={this.props.quantity < 1} clicked={this.props.removeDish}>-</Button>
+                        <p>{this.props.quantity}</p>
+                        <Button disable={this.props.quantity > 99} clicked={this.props.addDish}>+</Button>
+                    </div>
+                </div>
+                <div className={classes.Price}>
+                    ${this.props.price}
+                </div>
+                <div className={classes.Desc}>
+                    <p>Dish description...</p>
+                </div>
+            </div>
+        );
+    }
+};
+
+export default Dish;
