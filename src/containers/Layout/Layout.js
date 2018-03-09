776144f70b22dd9b726dee3076866302c1d7eb11
ravi2808 <ravi.cool2808@gmail.com>
2018-03-09 12:25:14 +0530

    Update Layout.js

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,1 +1,19 @@
+import React, { Component } from 'react';
 
+
+import Navigation from '../../components/Navigation/Navigation';
+import Cart from '../../components/UI/Cart/Cart';
+import classes from './Layout.css';
+
+class Layout extends Component {
+    render() {
+        return (
+            <header className={classes.Layout}>
+                <Navigation />
+                <Cart />
+            </header>
+        );
+    };
+}
+
+export default Layout;
