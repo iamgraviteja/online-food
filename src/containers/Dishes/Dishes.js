89add29690d44ba377e645a9b1e068298fae0cfc
ravi2808 <ravi.cool2808@gmail.com>
2018-03-12 17:35:05 +0530

    Update Dishes.js

--- src/containers/Dishes/Dishes.js
+++ src/containers/Dishes/Dishes.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Dish from './Dish/Dish';
+import Aux from '../../hoc/Auxilary';
 
 class Dishes extends Component {
     state = {
-        total: 0   
+        dishes: null,
+        total: 0
     }
-    // componentDidMount() {
-    //     console.log(this.state);
-    // }
-    // componentDidUpdate() {
-    //     console.log(this.state);
-    // }
+    componentDidMount() {
+        axios.get('https://react-order-food.firebaseio.com/dishes.json/101')
+            .then(response => {
+                console.log(response);
+                this.setState({ dishes: response.data })
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
     addDishHandler = (index) => {
 
         //Need to change later
@@ -41,29 +48,36 @@ class Dishes extends Component {
     render() {
 
         const dishesArray = [];
-        for (let key in this.props.dishes) {
+        for (let key in this.state.dishes) {
             dishesArray.push({
                 id: key,
-                details: this.props.dishes[key]
+                details: this.state.dishes[key]
             });
         }
+        let dishes = <p>Loading....</p>;
 
+        if (this.state.dishes) {
+            dishes = (
+                <div>
+                    {dishesArray.map((dish, index) => {
+                        return <Dish
+                            name={dish.details.name}
+                            price={dish.details.price}
+                            key={dish.id}
+                        // addDish={() => this.addDishHandler(index)}
+                        // removeDish={() => this.removeDishHandler(index)}
+                        />
+                    })}
+
+                    <p style={{ display: this.state.total > 0 ? 'block' : 'none' }}>Total: <strong>${this.state.total}</strong></p>
+                    <p>Dish count: {this.state.dishCount}</p>
+                </div>
+            );
+        }
         return (
-            <div>
-                {dishesArray.map((dish, index) => {
-                    return <Dish
-                        name={dish.details.name}
-                        price={dish.details.price}
-                        key={dish.id}
-                        quantity={dish.details.quantity}
-                    // addDish={() => this.addDishHandler(index)}
-                    // removeDish={() => this.removeDishHandler(index)}
-                    />
-                })}
-
-                <p style={{ display: this.state.total > 0 ? 'block' : 'none' }}>Total: <strong>${this.state.total}</strong></p>
-                <p>Dish count: {this.state.dishCount}</p>
-            </div>
+            <Aux>
+                {dishes}
+            </Aux>
         );
     }
 };
